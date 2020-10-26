c17f02110ad641a10c9a5f27e6db5ca8c22ae8c2
kumar-art <72661042+kumar-art@users.noreply.github.com>
2020-10-26 00:57:43 -0800

    Add files via upload

---
+++ festivals.js
@@ -0,0 +1,53 @@
+  
+var header=document.getElementById("main");
+var btns=header.getElementsByClassName("button1");
+for (var i=0;i<btns.length;i++){
+    btns[i].addEventListener("click",function(){
+        var current=document.getElementsByClassName("active","");
+        this.classname+="active";
+
+    });
+
+}
+
+
+function myfunction(){
+    window.location="jan.html";
+
+}
+function myfunction1(){
+    window.location="feb.html";
+}
+function myfunction2(){
+    window.location="mar.html";
+}
+function myfunction3(){
+    window.location="apri.html";
+}
+function myfunction4(){
+    window.location="may.html";
+}
+function myfunction5(){
+    window.location="jun.html";
+}
+function myfunction6(){
+    window.location="jul.html";
+}
+function myfunction7(){
+    window.location="aug.html";
+}
+function myfunction8(){
+    window.location="sep.html";
+}
+function myfunction9(){
+    window.location="oct.html";
+}
+function myfunction10(){
+    window.location="nov.html";
+}
+function myfunction11(){
+    window.location="dec.html";
+}
+function my6(){
+   window.location="mainpage.html";
+}
